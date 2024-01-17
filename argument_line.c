30d14416107f4a919cfed8e7059282743cdcd4da
Comfort99 <shingangecomfort@gmail.com>
2024-01-17 09:40:58 -0800

    monty

---
+++ argument_line.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * argument_line - Initializes a pointer to arg_s structure
+ */
+void argument_line(void)
+{
+	commands = malloc(sizeof(arg_t));
+	if (commands == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_commands();
+		exit(EXIT_FAILURE);
+	}
+	commands->instruction = malloc(sizeof(instruction_t));
+	if (commands->instruction == NULL)
+	{
+		fprint(stderr, "Error: malloc failed\n");
+		free_commands();
+		exit(EXIT_FAILURE);
+	}
+	commands->path = NULL;
+	commands->head = NULL;
+	commands->line = NULL;
+	commands->n_tokens = 0;
+	commands->line_number = 0;
+	commands->stack_length = 0;
+	commands->stack = 1;
+}
--- monty.h
+++ monty.h
@@ -22,15 +22,15 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struck commands_s - opcode and its function
+ * struck instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  */
-typedef struct commands_s
+typedef struct instruction_s
 {
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
-} commands_t;
+} instruction_t;
 
 /**
  * struct arg_s - hold variables
@@ -61,7 +61,7 @@ typedef struct arg_s
 	int stack;
 } arg_t;
 
-extern arg_t *arguments;
+extern arg_t *commands;
 
 void push(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
